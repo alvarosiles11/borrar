a335e8287522bb1de358e2d8c9249b5a96fe571e
Alvaro Siles <alvarosiles1@gmail.com>
2022-04-23 03:52:29 -0400

    Master
    Master

--- Application/Dto/Vuelo/VueloDto.java
+++ Application/Dto/Vuelo/VueloDto.java
@@ -3,21 +3,37 @@ package Application.Dto.Vuelo;
 import java.util.ArrayList;
 import java.util.UUID;
 
+import com.google.gson.Gson;
+
 public class VueloDto {
     public UUID Key;
-    // public String NroVuelo;
-    // private Gson Aeronave;
     private ArrayList<TripulanteDto> ListaTripulante;
     private ArrayList<AsientoDisponibleDto> ListaAsiento;
-    // private Gson AeropuertoOrigen;
-    // private Gson AeropuertoDestino;
-    // private Date Fecha_arribe;
-    // private Date Fecha_salida;
-    // private double Precio;
 
     public VueloDto() {
         ListaTripulante = new ArrayList<TripulanteDto>();
         ListaAsiento = new ArrayList<AsientoDisponibleDto>();
+    }
+
+    public ArrayList<TripulanteDto> getListaTripulante() {
+        return ListaTripulante;
+    }
+
+    public void setListaTripulante(ArrayList<TripulanteDto> listaTripulante) {
+        ListaTripulante = listaTripulante;
+    }
+
+    public ArrayList<AsientoDisponibleDto> getListaAsiento() {
+        return ListaAsiento;
+    }
+
+    public void setListaAsiento(ArrayList<AsientoDisponibleDto> listaAsiento) {
+        ListaAsiento = listaAsiento;
+    }
+
+    @Override
+    public String toString() {
+        return new Gson().toJson(this);
     }
 
 }
