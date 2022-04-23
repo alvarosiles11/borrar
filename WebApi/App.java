12f982e5ba861201b9e648e95c7535f9fd2a538b
Alvaro Siles <61075383+alvarosiles11@users.noreply.github.com>
2022-04-23 08:37:45 -0400

    master
    master

--- WebApi/App.java
+++ WebApi/App.java
@@ -1,9 +1,18 @@
 package WebApi;
+
+import Application.UseCases.Command.Vuelos.CrearVueloHandler;
+import Application.UseCases.Queries.GetVueloByKeyHandler;
 import kernel.http.Rest;
+import kernel.mediator.IMediator;
 
 public class App {
     public static void main(String[] args) throws Exception {
 
+        // Register handlers
+        IMediator.registerHandler(GetVueloByKeyHandler.class);
+        IMediator.registerHandler(CrearVueloHandler.class);
+
+        // Create the kernel
         Rest.addController(VueloApi.class);
         Rest.start();
 
--- WebApi/VueloApi.java
+++ WebApi/VueloApi.java
@@ -1,13 +1,13 @@
 package WebApi;
 
-// import org.json.JSONArray;
-// import org.json.JSONObject;
-
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
+import Application.Dto.Vuelo.VueloDto;
+import Application.UseCases.Command.Vuelos.CrearVueloCommand;
+import Application.UseCases.Queries.GetVueloByKeyQuery;
 import Domain.Model.Vuelos.Vuelo;
-import kernel.http.HttpStatus;
 import kernel.http.Exception.HttpException;
 import kernel.http.annotation.DeleteMapping;
 import kernel.http.annotation.GetMapping;
@@ -17,19 +17,6 @@ import kernel.http.annotation.PutMapping;
 import kernel.http.annotation.RequestBody;
 import kernel.http.annotation.RequestMapping;
 import kernel.http.annotation.RestController;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
-
-import Application.Dto.Vuelo.VueloDto;
-// import Application.Dto.AeronaveDto;
-// import Application.UseCases.Command.Aeronaves.CrearAeronaveCommand;
-// import Application.UseCases.Queries.Aeronaves.GetAeronaveByKeyQuery;
-import Domain.Model.Aeronaves.*;
-import kernel.http.HttpStatus;
-import kernel.http.Exception.HttpException;
-import kernel.http.annotation.*;
 import kernel.mediator.Mediator;
 import kernel.mediator.Response;
 
@@ -52,40 +39,31 @@ public class VueloApi {
         return vuelos;
     }
 
-    // @GetMapping("/{key}")
-    // public VueloDto getByKey(@PathVariable GetAeronaveByKeyQuery request) throws
-    // HttpException {
-    // AeronaveDto aeronaveDto = _mediator.send(request).data;
-    // return aeronaveDto;
-    // }
-
     @GetMapping("/{key}")
-    public Vuelo getByKey(@PathVariable String key) throws HttpException {
-        if (key.equals("1")) {
-            throw new HttpException(HttpStatus.CONFLICT, "Not found");
-        }
-        System.out.println("entro getByKey");
-        return new Vuelo();
+    public VueloDto getByKey(@PathVariable GetVueloByKeyQuery request) throws HttpException {
+        VueloDto vueloDto = _mediator.send(request).data;
+        return vueloDto;
     }
 
     @PostMapping("/registro")
-    public Vuelo register(@RequestBody Vuelo vuelo) {
+    public Response<UUID> register(@RequestBody CrearVueloCommand vuelo) {
+        Response<UUID> key = _mediator.send(vuelo);
+        // vuelo.agregarAsiento(new AsientoDisponible(vuelo.getKey(), 1, "primera"));
+        // vuelo.agregarAsiento(new AsientoDisponible(vuelo.getKey(), 2, "primera"));
 
-        // Gson aeve = new Gson(new Aeronave("bomfo", "2001"));
+        // Vuelo vuelo1 = new Vuelo();
+        // Aeronave aeronave = new Aeronave("Boeing", "777");
+        // vuelo1.AgregarAeronave(aeronave);
+        // vuelo1.AgregarAsientosDisponibles(new AsientoDisponible(aeronave.Key, "A1",
+        // "Comercial", 750));
+        // vuelo1.AgregarAsientosDisponibles(new AsientoDisponible(aeronave.Key, "A2",
+        // "Comercial", 750));
+        // vuelo1.AgregarAsientosDisponibles(new AsientoDisponible(aeronave.Key, "A3",
+        // "Comercial", 750));
+        // vuelo1.AgregarAsientosDisponibles(new AsientoDisponible(aeronave.Key, "A4",
+        // "Comercial", 750));
 
-        // JSONArray arr = new JSONArray();
-        // arr.put(new Gson().put("key", UUID.randomUUID().toString()).put("numero",
-        // "1"));
-        // arr.put(new Gson().put("key", UUID.randomUUID().toString()).put("numero",
-        // "2"));
-        // arr.put(new Gson().put("key", UUID.randomUUID().toString()).put("numero",
-        // "3"));
-        // arr.put(new Gson().put("key", UUID.randomUUID().toString()).put("numero",
-        // "4"));
-        // aeve.put("asientos", arr);
-
-        // vuelo.AgregarAeronave(aeve);
-        return vuelo;
+        return key;
     }
 
     @PutMapping("/{key}")
@@ -97,5 +75,4 @@ public class VueloApi {
     public void delete(@PathVariable String key) {
         System.out.println("entro delete");
     }
-
 }
