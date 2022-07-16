1e63bd4ef5f832becff2c53fd398fb08d6448b7b
alvarosiles11 <alvarosiles1@gmail.com>
2022-07-16 13:39:26 -0400

    ok
    pk

--- vuelo/index.js
+++ vuelo/index.js
@@ -1,7 +1,15 @@
+/** @format */
+
 function name(params) {
   alert(params);
 }
 
-<button className="prettier-class" id="prettier-id" onClick={this.handleClick}>
+<button
+  className='prettier-class'
+  id='prettier-id'
+  onClick={this.handleClick}
+  style={{
+    backgroundColor: 'red',
+  }}>
   Click Here
 </button>;
