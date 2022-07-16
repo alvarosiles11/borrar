9cbe9ccc8dd9a8f6627b7962269f6d6a45d7c2a8
alvarosiles11 <alvarosiles1@gmail.com>
2022-07-16 12:07:15 -0400

    master
    master

---
+++ vuelo/.prettierrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+	// tabWidth: 4,
+	// semi: false,
+	// singleQuote: true,
+
+
+	/*Especifique el número de espacios por nivel de sangría.*/
+	// tabWidth: 2,
+
+	/*Sangre las líneas con tabulaciones en lugar de espacios.*/
+	// useTabs: false,
+
+	/*Imprima punto y coma al final de las declaraciones.*/
+	// semi: true,
+
+	/*Utilice comillas simples en lugar de comillas dobles.*/
+	// singleQuote: true,
+
+
+	/*Use comillas simples en lugar de comillas dobles en JSX.*/
+	// jsxSingleQuote: true,
+
+
+	/*Sin comas finales*/
+	// trailingComma: "es5",
+
+	/*Quitar espacios entre corchetes en literales de objetos. {foo: bar} */
+	// bracketSpacing: false,
+
+	// Quitar espacios de elementos(HTML, JSX, Vue, Angular) dentro de <button>
+	// bracketSameLine: false,
+};
