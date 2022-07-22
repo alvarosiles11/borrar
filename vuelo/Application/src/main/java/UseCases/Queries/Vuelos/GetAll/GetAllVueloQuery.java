ec63efbf8ab3511a0cf5abf538cc29278c880717
alvarosiles11 <alvarosiles1@gmail.com>
2022-07-22 11:20:43 -0400

    borrador
    borrador

--- vuelo/Application/src/main/java/UseCases/Queries/Vuelos/GetAll/GetAllVueloQuery.java
+++
@@ -1,9 +0,0 @@
-package UseCases.Queries.Vuelos.GetAll;
-
-import Dto.VueloDto;
-import fourteam.mediator.Request;
-
-public class GetAllVueloQuery implements Request<VueloDto> {
-
-  public GetAllVueloQuery() {}
-}
--- vuelo/Domain/src/main/java/Repositories/IUnitOfWork.java
+++
@@ -1,7 +0,0 @@
-package Repositories;
-
-import fourteam.http.Exception.HttpException;
-
-public interface IUnitOfWork {
-  public void commit() throws HttpException;
-}
