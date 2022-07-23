37d2fce37923cfe3f1b8f6cf99b5f23c39388a04
alvarosiles11 <alvarosiles1@gmail.com>
2022-07-23 01:45:37 -0400

    test

---
+++ tets.js
@@ -0,0 +1,13 @@
+const salad = ['🍅', '🍄', '🥦', '🥒', '🌽', '🥕', '🥑'];
+salad[0];
+salad[2];
+salad[5];
+let greaterTen = [];
+for (let i = 0; i < numbers.length; i++) {
+  var currentNumber = numbers[i];
+  if (currentNumber > 10) {
+    greaterTen.push(currentNumber);
+  }
+}
+
+console.log(greaterTen);
